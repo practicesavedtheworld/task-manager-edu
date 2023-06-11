898ac5ea5632b12f0b3180a93d908d97779ff9f6
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-06-11 11:57:20 +0300

    add template

---
+++ template.py
@@ -0,0 +1,11 @@
+import aiohttp_jinja2
+import jinja2
+import pathlib
+from aiohttp import web
+
+T_PATH = pathlib.Path(__file__).parent / 'templates'
+
+
+def setup_jinja(app: web.Application):
+    loader = jinja2.FileSystemLoader(T_PATH)
+    aiohttp_jinja2.setup(app, loader=loader)

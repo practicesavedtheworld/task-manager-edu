26c30f6cbe533b23f8ff4702e4dd6c83fa7a6f39
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-06-11 11:44:42 +0300

    fix fernet err, add db,config

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from aiohttp import web
 from aiohttp_session import setup
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 from cryptography import fernet
+from settings import load_config
+from db import setup_db
 
 
 def main():
     app = web.Application()
-    setup(app, EncryptedCookieStorage(fernet.Fernet.generate_key()[32:]))
+    setup(app, EncryptedCookieStorage(fernet.Fernet.generate_key()[:32]))
+    config = load_config()
+    app['config'] = config
     loop = asyncio.get_event_loop()
+    loop.run_until_complete(setup_db(app=app))
     web.run_app(app=app, loop=loop, port=8888)
 
 

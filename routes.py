8f2e2d275540391a5c9de8b537d4b6ea29038580
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-06-11 21:15:41 +0300

    add routes bounded with views

--- routes.py
+++ routes.py
@@ -1,9 +1,24 @@
-from pathlib import Path
+import pathlib
+
 from aiohttp import web
-from views import login
+
+from views import login, another, menu, add_task, new_user, user_tasks, remove_task, edit, edit_status, my_tasks
+
 
 def setup_routes(app: web.Application):
     router = app.router
     router.add_get('/', login)
-    router.add_static('/js', Path(__file__).parent / 'js', name='js')
-    router.add_static('/static', Path(__file__).parent / 'static', name='static')
+    router.add_post('/new_user', new_user)
+
+    router.add_get('/menu', menu)
+    router.add_post('/go', another)
+    router.add_get('/my_tasks', my_tasks)
+    router.add_get('/finished_tasks', user_tasks)
+    router.add_post('/tasks', user_tasks)
+    router.add_post('/add', add_task)
+    router.add_put('/edit_status', edit_status)
+    router.add_put('/edit', edit)
+    router.add_delete('/remove', remove_task)
+
+    router.add_static('/js', pathlib.Path(__file__).parent / 'js', name='js')
+    router.add_static('/static', pathlib.Path(__file__).parent / 'static', name='static')

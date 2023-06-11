95483d0696e84a52791fa410fa32642fc22d00df
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-06-11 12:48:02 +0300

    add index route[get]

--- routes.py
+++ routes.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 from aiohttp import web
-
+from views import login
 
 def setup_routes(app: web.Application):
     router = app.router
+    router.add_get('/', login)
     router.add_static('/js', Path(__file__).parent / 'js', name='js')
     router.add_static('/static', Path(__file__).parent / 'static', name='static')

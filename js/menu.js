8d408864e99aa79877581d160d939724f4283b11
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-06-11 21:46:57 +0300

    updating, add mousehandler

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,16 @@
+function mouseMovementHandler(element, shadow) {
+    element.addEventListener('mouseenter', () => {
+        element.style.backgroundColor = shadow;
+        element.style.cursor = 'pointer';
+    })
+
+    element.addEventListener('mouseleave', () => {
+        element.style.backgroundColor = '';
+        element.style.cursor = 'default';
+    })
+}
+
+
 async function menuHandler() {
     // Shading SVG elements and tbody on hover
     let images = document.querySelectorAll('img');
